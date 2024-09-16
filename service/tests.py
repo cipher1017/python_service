f4022d9dded72f7bc580c62d1d67d78cd4dc1804
dev <kimanid488@gmail.com>
2024-09-16 18:30:42 +0300

    writing tests

--- service/tests.py
+++ service/tests.py
@@ -2,4 +2,30 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from .models import Customer, Order
 
-# Create your tests here.
+class ApiTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_customer(self):
+        # POST request to create a new customer
+        response = self.client.post('/api/customers/', {
+            'name': 'David',
+            'address': '123 Main Street',
+            'phone_number': '123-456-7890'
+        }, format='json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_order(self):
+        # customer to associate with the order
+        customer = Customer.objects.create(
+            name="David",
+            address="123 Main Street",
+            phone_number="+254748272022"
+        )
+        # POST request to create a new order for the customer
+        response = self.client.post('/api/orders/', {
+            'customer': customer.id,
+            'item': 'Bag',
+            'amount': 2300.50
+        }, format='json')
+        self.assertEqual(response.status_code, 201)
